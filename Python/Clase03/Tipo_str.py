48c35f6faca32e53066563d479ba0491edcbaf1a
RamiroMunoz <ramiromunoz2712@gmail.com>
2026-08-31 21:24:03 -0300

    Commit -> Clase03 Python

---
+++ Python/Clase03/Tipo_str.py
@@ -0,0 +1,12 @@
+# Profundizamos en el tipo String
+#Concatenación automática en py
+import math
+
+variable = ' adios'
+mensaje = 'hola ''alumnos' + variable
+mensaje += ', terminamos'
+#print(mensaje)
+
+#Usamos clase help para ayuda o documentación (built-in)
+#help(str)
+help(math.isnan)
