73cc832e90203af74ecb427b7ccd3bac9c10c69b
RamiroMunoz <ramiromunoz2712@gmail.com>
2026-09-17 00:57:56 -0300

    Commit Clase06 - Java Proyecto Libros

--- Java/Clase06/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Clase06/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,29 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.SpringApplication;
+import java.awt.EventQueue;
+
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import utn.tienda_libros.vista.LibroForm;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Desactivar el modo headless para habilitar Swing
+        ConfigurableApplicationContext contextoSpring =
+                new SpringApplicationBuilder(TiendaLibrosApplication.class)
+                        .headless(false)
+                        .web(WebApplicationType.NONE)
+                        .run(args);
 
+        // Ejecutar la ventana en el hilo de eventos de Swing (EDT)
+        EventQueue.invokeLater(() -> {
+            LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+            libroForm.setVisible(true);
+        });
+    }
 }
